3dc01079078312e67c49c17dea5eeeb50be82e75
ryanbender2 <52826517+ryanbender2@users.noreply.github.com>
2019-09-23 09:49:41 -0400

    finished version
    This is terrible, but it has some good things about it...

--- string_game.py
+++ string_game.py
@@ -16,13 +16,33 @@ def bar_chart(results):
     third_pos = [order_results[2][0], figures[str(order_results[2][0])], ' ' + str(order_results[2][1])]
     fourth_pos = [order_results[3][0], figures[str(order_results[3][0])], ' ' + str(order_results[3][1])]
 
+    """
+        I'm so sorry about all these checks.
+        there is a much, MUCH better way to do this, I'm just too lazy...
+    """
     if third_pos[2] == ' 0':
         third_pos[1] = '0'
         third_pos[2] = ''
     if fourth_pos[2] == ' 0':
         fourth_pos[1] = '0'
         fourth_pos[2] = ''
-
+    if first_pos[2] == second_pos[2]:
+        if first_pos[0][1] > second_pos[0][1]:
+            first_pos[0] = order_results[1][0]
+            second_pos[0] = order_results[0][0]
+    if (' ' + str(second_pos[2])) == third_pos[2]:
+        if second_pos[0][1] > third_pos[0][1]:
+            second_pos[0] = order_results[2][0]
+            third_pos[0] = order_results[1][0]        
+    if third_pos[2] == fourth_pos[2]:
+        if third_pos[0][1] > fourth_pos[0][1]:
+            third_pos[0] = order_results[3][0]
+            fourth_pos[0] = order_results[2][0] 
+            if third_pos[1] == '0' and fourth_pos[1] == '0':
+                third_pos[0] = order_results[3][0]
+                fourth_pos[0] = order_results[2][0]
+    # ending of the death checks
+    
     newstring =  "{}|{} {}\n{}|{} {}\n{}|{}{}\n{}|{}{}"\
         .format(first_pos[0], first_pos[1], first_pos[2],
                 second_pos[0], second_pos[1], second_pos[2],
@@ -30,7 +50,6 @@ def bar_chart(results):
                 fourth_pos[0], fourth_pos[1], fourth_pos[2])
 
     return newstring
-""" just need to figure out how to make the Q's come out in order if both 0 """
 
 
-print(bar_chart({'Q4': 0, 'Q3': 100, 'Q2': 0, 'Q1': 600}))
+print(bar_chart({'Q2': 0, 'Q3': 100, 'Q4': 0, 'Q1': 600}))
